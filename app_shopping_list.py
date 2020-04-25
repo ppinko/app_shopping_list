4922bfeb295161a588f159cda9887fb3945c31e5
ppinko <p.pinkowicz@gmail.com>
2020-04-25 19:03:55 +0200

    Adding option to store items from previous choices.

--- app_shopping_list.py
+++ app_shopping_list.py
@@ -18,6 +18,13 @@ root = tk.Tk()  # basis for the whole program. we attach everyhing to root
 root.title('Shopping list')
 
 items = []
+temp_items = None
+
+if os.path.isfile('shopping_list.txt'):
+    with open('shopping_list.txt', mode='r') as f:
+        temp_items = f.read()
+        temp_items = temp_items.split(',')
+        items = [x for x in temp_items if x.strip()]
 
 # initilize screen size
 canvas = tk.Canvas(root, height=600, width=600, bg='#263D42')
@@ -30,6 +37,10 @@ frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 # adding button to add a product to the list
 addItem = tk.Button(root, text='Add item', padx=10, pady=5, fg='black', bg='green', command=add_item)
 addItem.pack()
+
+for item in items:
+    label = tk.Label(frame, text=item, bg='white')
+    label.pack()
 
 root.mainloop() # run the program
 
