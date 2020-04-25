4dfac933465975eed96c83f1ad7f628981fdffd9
ppinko <p.pinkowicz@gmail.com>
2020-04-25 17:57:21 +0200

    Adding button to add items to shopping list.

--- app_shopping_list.py
+++ app_shopping_list.py
@@ -1,8 +1,21 @@
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import filedialog, Text, simpledialog
 import os
 
+
+def add_item():
+    item = simpledialog.askstring(title='', prompt="Item to add",
+                                    parent=root)
+    items.append(item)
+    for item in items:
+        label = tk.Label(frame, text=item, bg='white')
+        label.pack()
+
+
 root = tk.Tk()  # basis for the whole program. we attach everyhing to root
+root.title('Shopping list')
+
+items = []
 
 # initilize screen size
 canvas = tk.Canvas(root, height=600, width=600, bg='#263D42')
@@ -13,7 +26,8 @@ frame = tk.Frame(root, bg='white')
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 # adding button to add a product to the list
-addItem = tk.Button(root, text='Add item', padx=10, pady=5, fg='black', bg='green')
+addItem = tk.Button(root, text='Add item', padx=10, pady=5, fg='black', bg='green', command=add_item)
 addItem.pack()
+
 
 root.mainloop() # run the program
